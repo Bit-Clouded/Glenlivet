e6eaface61684e8ce24b6ba1bec231e3b1b3ad04
Jason Chen <jason.chen@bit-clouded.com>
2016-08-20 21:33:23 +1000

    Initial commit with the following mostly finished: Networking Elasticsearch processing of AWS log (To be properly verified) Script to completely test setting up everything, then tear everything down.

---
+++ New-PrivateDeployment.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    $bucketname = "bc-glenfiddich-local",
+    $projectname = "aws-lego",
+    $version = "test"
+)
+
+Set-DefaultAWSRegion ap-southeast-2
+.".\Deployment.ps1"
+$prefix = New-Deployment -bucketname $bucketname -projectname $projectname -version $version -deployroot ".\"
+
+Write-Host "Deployment s3 prefix: $prefix"
+
+return $prefix
---
+++ TestAllRegions.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    $bucketname = "bc-glenfiddich-local",
+    $projectname = "aws-lego",
+    $version = "integration-testing"
+)
+
+$prefix = .\New-PrivateDeployment.ps1 -bucketname $bucketname -projectname $projectname -version $version
+
+<#$regions = Get-AWSRegion
+$regions | % {
+    .\ExecuteAllTemplates.ps1 -region $_.Region -prefix $prefix
+}
+#>
