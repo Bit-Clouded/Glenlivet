f4e7fe34a2411e801430cf06a81dfc1479ad071d
Jason Chen <jason.chen@bit-clouded.com>
2016-10-08 11:47:03 +1100

    Finished script for testing all regions. Get region function from aws is not reliable sometimes. best to run with a set of tested regions.

--- TestAllRegions.ps1
+++ TestAllRegions.ps1
@@ -6,8 +6,19 @@
 
 $prefix = .\New-PrivateDeployment.ps1 -bucketname $bucketname -projectname $projectname -version $version
 
-<#$regions = Get-AWSRegion
+$regions = @(
+    @{region = "us-east-1"; ubuntuId = "" },
+    @{region = "us-west-1"; ubuntuId = "" },
+    @{region = "us-west-2"; ubuntuId = "" },
+    @{region = "eu-west-1"; ubuntuId = "" },
+    @{region = "eu-central-1"; ubuntuId = "" },
+    @{region = "ap-northeast-1"; ubuntuId = "" },
+    @{region = "ap-northeast-2"; ubuntuId = "" },
+    @{region = "ap-southeast-1"; ubuntuId = "" },
+    @{region = "ap-southeast-2"; ubuntuId = "" }
+    #@{region = "sa-east-1"; ubuntuId = "" }
+)
+
 $regions | % {
-    .\ExecuteAllTemplates.ps1 -region $_.Region -prefix $prefix
+    .\ExecuteAllTemplates.ps1 -region $_.region -prefix $prefix -ubuntuAmi $ami.ImageId
 }
-#>
