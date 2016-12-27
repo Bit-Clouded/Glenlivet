e47ff5822e40b3565ce02aec2bfeda7114bf4bb1
Jason Chen <jason.chen@bit-clouded.com>
2016-12-27 16:21:54 +1100

    Update to stack linking tentatively working in 2 regions. T146

---
+++ CleanUp.ps1
@@ -0,0 +1,40 @@
+﻿function SuperRemove-S3Bucket {
+    param($bucketName)
+    $region = (Get-S3BucketLocation -BucketName $bucketName).Value
+    Remove-S3BucketPolicy -BucketName $bucketName -Force -Region $region
+    Get-S3Version -BucketName $bucketName -Region $region | % {
+        $_.Versions | % {
+            Remove-S3Object -BucketName $bucketName -Key $_.Key -VersionId $_.VersionId -Force -Region $region
+        }
+    }
+    Remove-S3Bucket -BucketName $bucketName -Force -Region $region
+}
+
+Function CleanUp {
+    param([string]$region)
+    Set-DefaultAWSRegion $region
+
+    Get-CFNStack | ? {
+        $_.StackName.StartsWith("Bev-")
+    } | % {
+        Remove-CFNStack $_.StackId -Force
+    }
+}
+
+Get-S3Bucket | ? {
+    $_.BucketName.StartsWith("dev-elast")
+} | % {
+    SuperRemove-S3Bucket -bucketName $_.BucketName
+}
+
+CleanUp us-east-1
+CleanUp us-east-2
+CleanUp us-west-1
+CleanUp us-west-2
+CleanUp eu-west-1
+CleanUp eu-central-1
+CleanUp sa-east-1
+CleanUp ap-northeast-1 #<- no tag. bugging out.
+CleanUp ap-northeast-2
+CleanUp ap-southeast-1
+CleanUp ap-southeast-2
