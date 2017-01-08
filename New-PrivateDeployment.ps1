e3428086ca3867eee9744982e088247c381d1c4f
Jason Chen <jason.chen@bit-clouded.com>
2017-01-09 01:32:01 +1100

    Updates the cfnmeta dependencies block to remove the .template for sake of sanity. All templates will be refered to via template path.

--- New-PrivateDeployment.ps1
+++ New-PrivateDeployment.ps1
@@ -1,12 +1,12 @@
 ﻿param(
-    $bucketname = "local-glenfiddichbase-publicbucket-1mmtghaeejx68",
-    #$bucketname = "dev-glenfiddichbase-publicbucket-s2lgz460omgc",
+    #$bucketname = "local-glenfiddichbase-publicbucket-1mmtghaeejx68",
+    $bucketname = "dev-glenfiddichbase-publicbucket-s2lgz460omgc",
     $projectname = "glenlivet",
     $version = "test"
 )
 
-Set-DefaultAWSRegion ap-southeast-2
-#Set-DefaultAWSRegion eu-west-1
+#Set-DefaultAWSRegion ap-southeast-2
+Set-DefaultAWSRegion eu-west-1
 .".\Deployment.ps1"
 $prefix = New-Deployment -bucketname $bucketname -projectname $projectname -version $version -deployroot ".\"
 
