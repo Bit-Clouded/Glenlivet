f501ee77e54b312d277562f5ce2a59a878e72db7
Teakay <teakay.starwyn@gmail.com>
2019-08-30 12:25:59 +1000

    validation script
    Test Plan: phabricator
    
    Reviewers: jason.chen
    
    Reviewed By: jason.chen
    
    Differential Revision: https://phabricator.bit-clouded.io/D174

---
+++ Validate-Templates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+
+find "$(pwd -P)" -name "*.template" -print0 | while read -d $'\0' file
+do
+	if  [[ $file == *nat-subnets.template || $file == *mesosphere.template ]] ;
+	then
+		echo ''
+		echo '----!!!   nat-subnets amd mesosphere fail validation   !!!----'
+	else
+	    echo ""
+        echo "Validating $file";
+		aws cloudformation validate-template --template-body "file://$file"
+	fi
+done
