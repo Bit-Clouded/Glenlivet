f80d8c57a2a8603aecb4735d322f82bf7e9b3f78
Teakay <teakay.starwyn@gmail.com>
2019-09-02 13:12:19 +1000

    add cicd folder

---
+++ cicd/pull-validate-push.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+pwd
+
+
+git config --global user.email "ci@bit-clouded.com"
+
+git pull --no-edit ../../$internal master
+
+if [[ `git diff --check` ]]; then
+  echo 'Merge conflict!'
+  exit 1
+else
+  echo 'no merge conflict detected'
+fi
+
+bash cicd/validate-templates.sh
+
+if [[ $? -ne 0 ]]; then
+  echo "Validation failed!"
+  exit 1
+else
+  echo "Validation passed!"
+fi
+
+if [[ `git status --porcelain` ]]; then
+  git commit -m "merge"
+  git push origin master
+else
+  echo 'no changes to commit'
+fi
